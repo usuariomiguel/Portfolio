dee615e3a20f40683bdcbd00c84b88de14072adf
MiguelAngel <ansonfull@gmail.com>
2023-01-30 18:14:10 +0100

    radius gradient & header scroll

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 window.addEventListener('load', function () {
+
+    // Header Bg Section
+    let header = document.querySelector(".header");
+
+    const functionChangeBgHeader = ()=> {
+    if(document.documentElement.scrollTop >= 30) {
+        header.classList.add("addColor");
+    } else {
+        header.classList.remove("addColor");
+        // navList__linkSkills.classList.remove("active");
+        // navList__linkAbout.classList.remove("active");
+        // navList__linkProjects.classList.remove("active");
+        // navList__linkContact.classList.remove("active");
+        }   
+    }
+    window.addEventListener("scroll" , functionChangeBgHeader);
+
     let navBurguermenu__link = this.document.querySelector(".navBurguermenu__link");
     let navList__movile = this.document.querySelector(".navList__movile");
     let main = this.document.querySelector(".main");

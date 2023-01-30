0c3ee9bb1caed5da9caaffab16288050ff76ebb0
MiguelAngel <ansonfull@gmail.com>
2023-01-30 17:46:24 +0100

    menu movil

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ window.addEventListener('load', function () {
     let navList__movile = this.document.querySelector(".navList__movile");
     let main = this.document.querySelector(".main");
     navBurguermenu__link.addEventListener('click', function () {
+        setTimeout(() => {
+            main.classList.toggle("active");
+        }, 200)
         navList__movile.classList.toggle("active");
-        main.classList.toggle("active");
     })
 })

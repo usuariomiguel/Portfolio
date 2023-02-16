c8929ed533de93bc863c4da8a9e1832522fdb0f3
MiguelAngel <ansonfull@gmail.com>
2023-02-16 13:38:52 +0100

    bg and colors

--- main.js
+++ main.js
@@ -127,4 +127,42 @@ window.addEventListener('load', function () {
         habContainer__lenguajes.classList.remove("active");
         habContainer__utility.classList.add("active");
     })
+    // technique for this demo found here 
+    // http://stackoverflow.com/questions/22003491/animating-canvas-to-look-like-tv-noise
+
+    const canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d')
+
+    canvas.width = canvas.height = 128
+
+    resize();
+    window.onresize = resize;
+
+    function resize() {
+    canvas.width = window.innerWidth * window.devicePixelRatio
+    canvas.height = window.innerHeight * window.devicePixelRatio
+    canvas.style.width = window.innerWidth + 'px'
+    canvas.style.height = window.innerHeight + 'px'
+    }
+
+    function noise(ctx) {
+
+    const w = ctx.canvas.width,
+    h = ctx.canvas.height,
+    iData = ctx.createImageData(w, h),
+    buffer32 = new Uint32Array(iData.data.buffer),
+    len = buffer32.length
+    let i = 0
+
+    for(; i < len;i++)
+
+    if (Math.random() < 0.05) buffer32[i] = 0xffffffff;
+
+    ctx.putImageData(iData, 0, 0);
+    }
+
+    (function loop() {
+    noise(ctx);
+    requestAnimationFrame(loop);
+    })();
 })

8a491a91c4c429ff851ea79f9a864c4d3bc837db
MiguelAngel <ansonfull@gmail.com>
2023-02-22 17:55:05 +0100

    cambios

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function () {
 
-        // Header Bg Section
-        let header = document.querySelector(".header");
-        const functionChangeBgHeader = ()=> {
-        if(document.documentElement.scrollTop >= 30) {
+    // Header Bg Section
+    let header = document.querySelector(".header");
+    const functionChangeBgHeader = () => {
+        if (document.documentElement.scrollTop >= 30) {
             header.classList.add("addColor");
         } else {
             header.classList.remove("addColor");
@@ -11,42 +11,32 @@ window.addEventListener('load', function () {
             // navList__linkAbout.classList.remove("active");
             // navList__linkProjects.classList.remove("active");
             // navList__linkContact.classList.remove("active");
-            }   
         }
-        window.addEventListener("scroll" , functionChangeBgHeader);
-        
-        let navBurguermenu__link__open = this.document.querySelector(".navBurguermenu__link--1");
-        let navBurguermenu__link__close = this.document.querySelector(".navBurguermenu__link--2");
-        let navBurguermenu__img__hamburguer = this.document.querySelector(".navBurguermenu__img__hamburguer");
-        let navBurguermenu__img__close = this.document.querySelector(".navBurguermenu__img__close");
-        let navList__movile = this.document.querySelector(".navList__movile");
-        let main = this.document.querySelector(".main");
-        let footer = this.document.querySelector(".footer");
-        navBurguermenu__link__open.addEventListener('click', function () {
-            setTimeout(() => {
-                main.classList.add("active");
-                navBurguermenu__img__close.classList.add("active");
-            }, 200)
-            navList__movile.classList.add("active");
-            footer.classList.add("active");
-            navBurguermenu__img__hamburguer.classList.add("active");
-        })
+    }
+    window.addEventListener("scroll", functionChangeBgHeader);
 
-        navBurguermenu__link__close.addEventListener('click', function () {
-            setTimeout(() => {
-                navBurguermenu__img__close.classList.remove("active");
-                navBurguermenu__img__hamburguer.classList.remove("active");
-                main.classList.remove("active");
-            }, 200)
-            navList__movile.classList.remove("active");
-            footer.classList.remove("active");
-        })
-    
+    let navBurguermenu__link__open = this.document.querySelector(".navBurguermenu__link--1");
+    let navBurguermenu__link__close = this.document.querySelector(".navBurguermenu__link--2");
+    let navBurguermenu__img__hamburguer = this.document.querySelector(".navBurguermenu__img__hamburguer");
+    let navBurguermenu__img__close = this.document.querySelector(".navBurguermenu__img__close");
+    let navList__movile = this.document.querySelector(".navList__movile");
+    navBurguermenu__link__open.addEventListener('click', function () {
+        navBurguermenu__img__close.classList.add("active");
+        navBurguermenu__img__hamburguer.classList.add("active");
+        navList__movile.classList.add("active");
+    })
+
+    navBurguermenu__link__close.addEventListener('click', function () {
+        navBurguermenu__img__close.classList.remove("active");
+        navBurguermenu__img__hamburguer.classList.remove("active");
+        navList__movile.classList.remove("active");
+    })
+
     let navList__menu__hero = document.querySelector(".navList__menu__hero");
     let navList__menu__portfolio = document.querySelector(".navList__menu__portfolio");
     let navList__menu__skills = document.querySelector(".navList__menu__skills");
     let navList__menu__form = document.querySelector(".navList__menu__form");
-    
+
     navList__menu__hero.addEventListener('click', function () {
         navBurguermenu__img__close.classList.remove("active");
         navBurguermenu__img__hamburguer.classList.remove("active");
@@ -118,7 +108,7 @@ window.addEventListener('load', function () {
         habContainer__lenguajes.classList.add("active");
         habContainer__utility.classList.remove("active");
     })
-    
+
     tools.addEventListener("click", function () {
         tools.classList.add("active");
         All.classList.remove("active");

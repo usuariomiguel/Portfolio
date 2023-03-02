c90cafc6a8d629af88a51826625a7db50de754d2
MiguelAngel <ansonfull@gmail.com>
2023-03-02 02:10:02 +0100

    hero

---
+++ vanta.js
@@ -0,0 +1,13 @@
+VANTA.GLOBE({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xf23073,
+    color2: 0x891e1e,
+    backgroundColor: 0x291350
+  })

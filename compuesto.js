7e7c3d2634b0d3dbb07b26545e7543b33fc65eda
usuariomiguel <ansonfull@gmail.com>
2024-07-12 14:02:06 +0200

    js

--- compuesto.js
+++ compuesto.js
@@ -5,11 +5,18 @@ window.addEventListener('load', function () {
     let input_period = document.getElementById("input-period");
     let input_interes = document.getElementById("input-interes");
     let input_duracion = document.getElementById("input-duracion");
+    let option_periodico = document.getElementById("periodo");
+    let dia = document.getElementById("dia");
+    let semana = document.getElementById("semana");
+    let mes = document.getElementById("mes");
+    let año = document.getElementById("año");
     let submit_calcular = document.getElementById("submit");
 
+
     let resultado_balance = document.getElementById("r-bal");
-    let resultado_periodico = document.getElementById("r-pep");
+    let resultado_periodico = document.getElementById("r-per");
     let resultado_total = document.getElementById("r-tot");
+    let resultado_totalt = document.getElementById("r-tott");
 
     submit_calcular.addEventListener('click', function () {
         let inicial = parseFloat(input_inicial.value);
@@ -17,12 +24,23 @@ window.addEventListener('load', function () {
         let interes = parseFloat(input_interes.value);
         let duracion = parseFloat(input_duracion.value);
 
-        let periodico = period * 12 * duracion;
-        let total = inicial + (periodico * interes);
+        let interest = (inicial + periodico) * interes / 100;
+        let total = interest + inicial + periodico
 
         resultado_balance.innerHTML = inicial + '€';
         resultado_periodico.innerHTML = periodico + '€';
-        resultado_total.innerHTML = total + '€';
+        resultado_total.innerHTML = interest + '€';
+        resultado_totalt.innerHTML = total + '€';
+
+        if (dia.value == "Diario") {
+            let periodico = (period * 365) * duracion;
+        } else if (semana.value == "Semanal") {
+            let periodico = (period * (4 * 12)) * duracion;
+        } else if (mes.value == "Mensual") {
+            let periodico = (period * 12) * duracion;
+        } else if (año.value == "Anual") {
+            let periodico = period * duracion;
+        }
     })    
 
 })

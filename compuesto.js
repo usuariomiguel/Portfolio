e92969907466a4ae7da113eea2b95840d82ee9d7
usuariomiguel <ansonfull@gmail.com>
2024-07-15 13:30:31 +0200

    interes compuesto calculo

--- compuesto.js
+++ compuesto.js
@@ -24,7 +24,20 @@ window.addEventListener('load', function () {
         } else {
             if (option_periodico.value == "Diario") {
                 let periodico = (period * 365) * duracion;
-                let interest = (inicial + periodico) * interes / 100;
+                let i1 = (inicial + periodico) * (interes / 100) / 12;
+                let i2 = (inicial + periodico + i1) * (interes / 100) / 12;
+                let i3 = (inicial + periodico + i1 + i2) * (interes / 100) / 12;
+                let i4 = (inicial + periodico + i1 + i2 + i3) * (interes / 100) / 12;
+                let i5 = (inicial + periodico + i1 + i2 + i3+ i4) * (interes / 100) / 12;
+                let i6 = (inicial + periodico + i1 + i2 + i3+ i4 + i5) * (interes / 100) / 12;
+                let i7 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6) * (interes / 100) / 12;
+                let i8 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7) * (interes / 100) / 12;
+                let i9 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8) * (interes / 100) / 12;
+                let i10 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9) * (interes / 100) / 12;
+                let i11 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9 + i10) * (interes / 100) / 12;
+                let i12 = (inicial + periodico + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11) * (interes / 100) / 12;    
+                console.log(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9 , i10 , i11 , i12)
+                let interest = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12;
                 let total = interest + inicial + periodico;
                 document.getElementById("r-bal").innerHTML = inicial.toFixed(2) + '€';
                 document.getElementById("r-per").innerHTML = periodico.toFixed(2) + '€';
@@ -32,28 +45,67 @@ window.addEventListener('load', function () {
                 document.getElementById("r-tott").innerHTML = total.toFixed(2) + '€';
             } else if (option_periodico.value == "Semestral") {
                 let periodico = (period * (4 * 12)) * duracion;
-                let interest = (inicial + periodico) * interes / 100;
+                let i1 = (inicial + periodico) * (interes / 100) / 12;
+                let i2 = (inicial + periodico + i1) * (interes / 100) / 12;
+                let i3 = (inicial + periodico + i1 + i2) * (interes / 100) / 12;
+                let i4 = (inicial + periodico + i1 + i2 + i3) * (interes / 100) / 12;
+                let i5 = (inicial + periodico + i1 + i2 + i3+ i4) * (interes / 100) / 12;
+                let i6 = (inicial + periodico + i1 + i2 + i3+ i4 + i5) * (interes / 100) / 12;
+                let i7 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6) * (interes / 100) / 12;
+                let i8 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7) * (interes / 100) / 12;
+                let i9 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8) * (interes / 100) / 12;
+                let i10 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9) * (interes / 100) / 12;
+                let i11 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9 + i10) * (interes / 100) / 12;
+                let i12 = (inicial + periodico + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11) * (interes / 100) / 12;    
+                console.log(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9 , i10 , i11 , i12)
+                let interest = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12;
                 let total = interest + inicial + periodico;
-                document.getElementById("r-bal").innerHTML = inicial + '€';
-                document.getElementById("r-per").innerHTML = periodico + '€';
-                document.getElementById("r-tot").innerHTML = interest + '€';
-                document.getElementById("r-tott").innerHTML = total + '€';
+                document.getElementById("r-bal").innerHTML = inicial.toFixed(2) + '€';
+                document.getElementById("r-per").innerHTML = periodico.toFixed(2) + '€';
+                document.getElementById("r-tot").innerHTML = interest.toFixed(2) + '€';
+                document.getElementById("r-tott").innerHTML = total.toFixed(2) + '€';
             } else if (option_periodico.value == "Mensual") {
                 let periodico = (period * 12) * duracion;
-                let interest = (inicial + periodico) * interes / 100;
+                let i1 = (inicial + periodico) * (interes / 100) / 12;
+                let i2 = (inicial + periodico + i1) * (interes / 100) / 12;
+                let i3 = (inicial + periodico + i1 + i2) * (interes / 100) / 12;
+                let i4 = (inicial + periodico + i1 + i2 + i3) * (interes / 100) / 12;
+                let i5 = (inicial + periodico + i1 + i2 + i3+ i4) * (interes / 100) / 12;
+                let i6 = (inicial + periodico + i1 + i2 + i3+ i4 + i5) * (interes / 100) / 12;
+                let i7 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6) * (interes / 100) / 12;
+                let i8 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7) * (interes / 100) / 12;
+                let i9 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8) * (interes / 100) / 12;
+                let i10 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9) * (interes / 100) / 12;
+                let i11 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9 + i10) * (interes / 100) / 12;
+                let i12 = (inicial + periodico + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11) * (interes / 100) / 12;    
+                console.log(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9 , i10 , i11 , i12)
+                let interest = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12;
                 let total = interest + inicial + periodico;
-                document.getElementById("r-bal").innerHTML = inicial + '€';
-                document.getElementById("r-per").innerHTML = periodico + '€';
-                document.getElementById("r-tot").innerHTML = interest + '€';
-                document.getElementById("r-tott").innerHTML = total + '€';
+                document.getElementById("r-bal").innerHTML = inicial.toFixed(2) + '€';
+                document.getElementById("r-per").innerHTML = periodico.toFixed(2) + '€';
+                document.getElementById("r-tot").innerHTML = interest.toFixed(2) + '€';
+                document.getElementById("r-tott").innerHTML = total.toFixed(2) + '€';
             } else if (option_periodico.value == "Anual") {
                 let periodico = period * duracion;
-                let interest = (inicial + periodico) * interes / 100;
+                let i1 = (inicial + periodico) * (interes / 100) / 12;
+                let i2 = (inicial + periodico + i1) * (interes / 100) / 12;
+                let i3 = (inicial + periodico + i1 + i2) * (interes / 100) / 12;
+                let i4 = (inicial + periodico + i1 + i2 + i3) * (interes / 100) / 12;
+                let i5 = (inicial + periodico + i1 + i2 + i3+ i4) * (interes / 100) / 12;
+                let i6 = (inicial + periodico + i1 + i2 + i3+ i4 + i5) * (interes / 100) / 12;
+                let i7 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6) * (interes / 100) / 12;
+                let i8 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7) * (interes / 100) / 12;
+                let i9 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8) * (interes / 100) / 12;
+                let i10 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9) * (interes / 100) / 12;
+                let i11 = (inicial + periodico + i1 + i2 + i3+ i4 + i5 + i6 + i7 + i8 + i9 + i10) * (interes / 100) / 12;
+                let i12 = (inicial + periodico + i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11) * (interes / 100) / 12;    
+                console.log(i1 , i2 , i3 , i4 , i5 , i6 , i7 , i8 , i9 , i10 , i11 , i12)
+                let interest = i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12;
                 let total = interest + inicial + periodico;
-                document.getElementById("r-bal").innerHTML = inicial + '€';
-                document.getElementById("r-per").innerHTML = periodico + '€';
-                document.getElementById("r-tot").innerHTML = interest + '€';
-                document.getElementById("r-tott").innerHTML = total + '€';
+                document.getElementById("r-bal").innerHTML = inicial.toFixed(2) + '€';
+                document.getElementById("r-per").innerHTML = periodico.toFixed(2) + '€';
+                document.getElementById("r-tot").innerHTML = interest.toFixed(2) + '€';
+                document.getElementById("r-tott").innerHTML = total.toFixed(2) + '€';
             }
         }
 
